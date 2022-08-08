54da8cb15679fb430a8558a62370e66243ba4519
logickoder <chukwudumebiorazulike@gmail.com>
2022-08-08 12:42:34 +0100

    initial commit

---
+++ app/src/main/java/dev/logickoder/synote/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package dev.logickoder.synote.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(2.dp),
+    medium = RoundedCornerShape(5.dp),
+    large = RoundedCornerShape(10.dp)
+)
