39e7ec86b8dbb460fad90f58ba5ab83dfe872c10
logickoder <chukwudumebiorazulike@gmail.com>
2022-08-09 16:50:29 +0100

    added splash screen

--- app/src/main/java/dev/logickoder/synote/MainActivity.kt
+++ app/src/main/java/dev/logickoder/synote/MainActivity.kt
@@ -10,10 +10,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dev.logickoder.synote.core.theme.SynoteTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Handle the splash screen transition.
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         setContent {
             SynoteTheme {
